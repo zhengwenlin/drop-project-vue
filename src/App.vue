8c6871f9d7f7bd2aa48ca432da2fb7aaad96cd47
zhengwenlin <zhengwl@tellhow.com>
2021-02-01 11:32:16 +0800

    '.'

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent }    from 'vue';
 import VisualEditor from './packags/visual-editor';
 import {visualConfig} from './visual.config'
 export default defineComponent({
